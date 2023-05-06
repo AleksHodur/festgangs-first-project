7c33155d9abcd4ce5cd58f14639d295af33c7148
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2023-05-06 15:22:34 +0200

    Añadida tabla tipos usuario

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const userModel = require('./models/userModel');
+//const userModel = require('./models/userModel');
 
 const conexionBD = mysql.createConnection({
     host: 'localhost',
@@ -22,28 +22,54 @@ conexionBD.query('CREATE DATABASE festgangs;', (err, result) => {
     console.log('Base de datos creada con éxito');
 });
 
+conexionBD.query('DROP TABLE IF EXISTS festgangs.usertype;', (err, result) => {
+    if (err) throw err;
+    console.log('Result: ' + result);
+});
+
 conexionBD.query('DROP TABLE IF EXISTS festgangs.user;', (err, result) => {
     if (err) throw err;
     console.log('Result: ' + result);
 });
 
-let crearTablaUser = 'CREATE TABLE festgangs.user (' +
+const crearTablaUserType = 'CREATE TABLE festgangs.usertype (' +
+                            'id INT AUTO_INCREMENT PRIMARY KEY,' +
+                            'name VARCHAR(30) NOT NULL,' +
+                            'description VARCHAR(200))';
+
+conexionBD.query(crearTablaUserType, (err, result) => {
+    if(err) throw err;
+    console.log('Tabla usertype creada con éxito');
+});
+
+const llenarTablaUserType = 'INSERT INTO festgangs.usertype (name, description) VALUES ' +
+                            "('standard', 'Poderes: crear perfil personal, organizar/partcipar en grupos/eventos, buscar información sobre estos'), " +
+                            "('moderator', 'Poderes: borrar mensajes, borrar fotos, banear usuarios'), " +
+                            "('administrator', 'Poderes: editar eventos, gestionar usuarios')";
+
+conexionBD.query(llenarTablaUserType, (err, result) => {
+    if (err) throw err;
+    console.log('Tabla userType rellenada con éxito');
+});
+
+const crearTablaUser = 'CREATE TABLE festgangs.user (' +
                     'id INT AUTO_INCREMENT PRIMARY KEY,' +
+                    'type INT NOT NULL,' +
                     'email VARCHAR(50) NOT NULL,' +
                     'name VARCHAR(30) NOT NULL,' +
                     'password VARCHAR(30) NOT NULL' +
                     ');';
 
-console.log(crearTablaUser);
+//console.log(crearTablaUser);
 
 conexionBD.query(crearTablaUser, (err, result) => {
     if (err) throw err;
     console.log('Tabla user creada con éxito');
 });
 
-let llenarTablaUser = 'INSERT INTO festgangs.user (email, name, password) VALUES' +
-                    "('correo@basic.es', 'Basic Martínez', '1234')," +
-                    "('juan@gmail.com', 'Juan Coppola', '4321');";
+const llenarTablaUser = 'INSERT INTO festgangs.user (type, email, name, password) VALUES' +
+                    "(1, 'correo@basic.es', 'Basic Martínez', '1234')," +
+                    "(1, 'juan@gmail.com', 'Juan Coppola', '4321');";
 
 console.log(llenarTablaUser);
 
@@ -52,20 +78,20 @@ conexionBD.query(llenarTablaUser, (err, result) => {
     console.log('Tabla user rellenada con éxito');
 });
 
-let userId = 2;
+/* let userId = 2;
 /*
 conexionBD.query('SELECT * FROM festgangs.user WHERE id = ?;', [userId], (err, result, fields) => {
     if (err) throw err;
     console.log(result);
     console.log(fields);
-}); */
+}); 
 
 conexionBD.query('SELECT * FROM festgangs.user WHERE id = ?;', [userId], (err, result, fields) => {
     if (err) throw err;
 /*     console.log(result);
-    console.log(result[0].id); */
+    console.log(result[0].id);
     console.log(result);
     let field = result[0];
     let user = userModel(field.id, field.email, field.name, field.password);
     console.log(user.saludo());
-});
+}); */
