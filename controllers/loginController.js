f4bcd14d35e1b974485e9f005089befdfb2c60bc
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2023-05-06 14:54:53 +0200

    Limpiando y renombrando

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,7 +14,7 @@ conexionBD.connect((err) => {
   console.log('Connected to db! :)');
 });
 
-const login_index = (request, response) => {response.render('sesion', {title: 'Login'});};
+const login_index = (request, response) => {response.render('login', {title: 'Login'});};
 
 const login_close = (request, response) => {
     request.session.destroy();
@@ -23,31 +23,7 @@ const login_close = (request, response) => {
 
 const login_check = async (request, response) => {
     
-
     const { email, password } = request.body;
-  /*
-    if (!email || !password) {
-        response.status(400).json({error: 'Missing email or password'});
-        return;
-    }
-  
-    try {
-        conexionBD.query("SELECT * FROM festgangs.user WHERE email = ? AND password = ?", [email, password], (err, result, fields) => {
-  
-          if(err || result.length < 1){
-            response.status(200).json({found: false, message: 'El usuario o la contraseña son incorrectos'});
-          }else{
-            let fields = result[0];
-            request.session.user = userModel(fields.id, fields.email, fields.name, fields.password);
-            response.status(200).json({found: true, message: 'Usuario correcto!'});
-          }
-        });
-  
-    } catch (error) {
-        console.error(error);
-        response.status(500).json({error: 'Internal server error'});
-    } */
-
 
     try{
       console.log(userDAO);
