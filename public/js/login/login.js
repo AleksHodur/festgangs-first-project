e55397ccf8b4a02230ca4d1c4b4da4975e5d3428
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2023-05-07 21:00:28 +0200

    Problema imagenes

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -1,7 +1,7 @@
 //Poner una imagen aleatoria de fondo
 let number = Math.floor(Math.random() * 6) + 1;
 //let number = 1;
-let imagePath = "url('/img/login/login" + number + ".webp')";
+let imagePath = "url('/img/login/login" + number + ".jpg')";
 $('body').css("background-image", imagePath)
 
 const botonInicio = $('#inicioSesion');
